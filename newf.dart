a5914838b03bed180c540973a16c04c56e57351b
UMI MADIHAH BT MOHAMED RAIMI <47856883+madihahdihah97@users.noreply.github.com>
2020-12-06 16:13:26 +0800

    Create newf.dart

---
+++ newf.dart
@@ -0,0 +1,6 @@
+class Found {
+    final String item , color, wherefound, datefound, deliverymethod, foundby;
+    Found({
+      this.item, this.color, this.wherefound, this.datefound, this.deliverymethod, this.foundby
+    });
+}
