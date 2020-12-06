14d422c3ea89c158f659c3d8c162afb31795b53c
UMI MADIHAH BT MOHAMED RAIMI <47856883+madihahdihah97@users.noreply.github.com>
2020-12-06 16:11:58 +0800

    Create modelf.dart

---
+++ modelf.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Model {
+  String item;
+  String color;
+  String wheref;
+  String datef;
+  String fby;
+  String deliverymethod;
+  String email;
+  String password;
+
+  Model({this.item, this.color, this.wheref, this.datef, this.fby, this.deliverymethod, this.email, this.password});
+}
