d62acfb4ab4a468641106ad4e812beab45ccc7d6
UMI MADIHAH BT MOHAMED RAIMI <47856883+madihahdihah97@users.noreply.github.com>
2020-12-06 15:45:20 +0800

    Create lofossf.dart

---
+++ lofossf.dart
@@ -0,0 +1,13 @@
+class Lofossf {
+
+  final String item;
+  final String color;
+  final String wherefound;
+  final String datefound;
+  final String deliverymethod;
+  final String foundby;
+
+
+  Lofossf({ this.item, this.color, this.wherefound, this.datefound, this.deliverymethod, this.foundby});
+  
+}
