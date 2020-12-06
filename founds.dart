2d67e296b25f0d22c748e037a3fa6e732b623a09
UMI MADIHAH BT MOHAMED RAIMI <47856883+madihahdihah97@users.noreply.github.com>
2020-12-06 16:02:12 +0800

    Create founds.dart

---
+++ founds.dart
@@ -0,0 +1,6 @@
+class Founds {
+  final String typeItem, colorOrmodel, wherefound, datefound, foundby, deliverymethod;
+  Founds({ this.typeItem, this.colorOrmodel, this.wherefound, this.datefound,this.foundby,this.deliverymethod,
+  });
+  
+}
