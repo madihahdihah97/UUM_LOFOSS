ddb8a2943599a1964618cedf1247b19da1a50871
UMI MADIHAH BT MOHAMED RAIMI <47856883+madihahdihah97@users.noreply.github.com>
2020-12-06 15:46:31 +0800

    Create user.dart

---
+++ user.dart
@@ -0,0 +1,20 @@
+class User{
+
+final String uid;
+
+User({this.uid});
+
+}
+
+
+class UserData {
+
+  final String uid;
+  final String item;
+  final String color;
+  final String wherefound;
+  final String datefound;
+
+  UserData({ this.uid, this.item, this.color, this.wherefound, this.datefound });
+  
+}
