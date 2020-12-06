db4780ec49781007f6f418d247fc4cb8031b95f2
UMI MADIHAH BT MOHAMED RAIMI <47856883+madihahdihah97@users.noreply.github.com>
2020-12-06 16:09:06 +0800

    Create Lost.dart

---
+++ Lost.dart
@@ -0,0 +1,6 @@
+class Lost {
+    final String name , number, item, datelost, wherelost, colorlost;
+    Lost({
+      this.name, this.number, this.item, this.datelost, this.wherelost, this .colorlost,
+    });
+}
