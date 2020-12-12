92fd48e96a32f9ae053bf195f02d73fdbb6d380f
UMI MADIHAH BT MOHAMED RAIMI <47856883+madihahdihah97@users.noreply.github.com>
2020-12-12 20:02:27 +0800

    Update authenticate.dart

--- authenticate.dart
+++ authenticate.dart
@@ -8,22 +8,17 @@ class Authenticate extends StatefulWidget {
   @override
   _AuthenticateState createState() => _AuthenticateState();
 }
-
 class _AuthenticateState extends State<Authenticate> {
-
 bool showSignIn = true;
 void toggleView(){
   setState(()=>showSignIn=false);
 }
-
   @override
   Widget build(BuildContext context) {
     if(showSignIn){
       return SignIn(toggleView: toggleView);
     }else{
       return Register(toggleView: toggleView);
-    }
-      
-    
+    }  
   }
 }
