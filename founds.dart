e30fd96ecd8b9c3b70573702367e23a9619f1c01
UMI MADIHAH BT MOHAMED RAIMI <47856883+madihahdihah97@users.noreply.github.com>
2020-12-13 00:30:09 +0800

    Update founds.dart

--- founds.dart
+++ founds.dart
@@ -2,5 +2,4 @@ class Founds {
   final String typeItem, colorOrmodel, wherefound, datefound, foundby, deliverymethod;
   Founds({ this.typeItem, this.colorOrmodel, this.wherefound, this.datefound,this.foundby,this.deliverymethod,
   });
-  
 }
