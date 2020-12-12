4cf252312af61b1756a694ddfe1e7e1c907be502
UMI MADIHAH BT MOHAMED RAIMI <47856883+madihahdihah97@users.noreply.github.com>
2020-12-13 00:26:55 +0800

    Update Lost.dart

--- Lost.dart
+++ Lost.dart
@@ -2,5 +2,5 @@ class Lost {
     final String name , number, item, datelost, wherelost, colorlost;
     Lost({
       this.name, this.number, this.item, this.datelost, this.wherelost, this .colorlost,
-    });
+   });
 }
