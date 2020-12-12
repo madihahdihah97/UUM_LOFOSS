9a617134e46f883fb11dcedeac8576f526f39bbc
UMI MADIHAH BT MOHAMED RAIMI <47856883+madihahdihah97@users.noreply.github.com>
2020-12-12 19:32:56 +0800

    Update main.dart

--- main.dart
+++ main.dart
@@ -8,28 +8,24 @@ import 'package:uumlofoss/screenlofoss/home/found.dart';
 import 'package:uumlofoss/screenlofoss/home/model.dart';
 import 'package:uumlofoss/screenlofoss/home/resultf.dart';
 import 'package:uumlofoss/screenlofoss/home/piclost.dart';
+import 'package:splashscreen/splashscreen.dart';
 
 void main()=>runApp(MyApp());
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return StreamProvider<User>.value(
       value: AuthService().user,
           child: MaterialApp(
-        
         debugShowCheckedModeBanner: false,  
         title: 'Flutter Database',
         theme: ThemeData(
           primaryColor:Colors.blue[900],
           accentColor:Colors.blue[100],
         ), 
-
         home: Wrapper(),
       ),
-    );
-      
-    
+    ); 
   }
 }
   
